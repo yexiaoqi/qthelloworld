9a28f91ec90f3645b27704979f9956439fef9c6c
yexiaoqi <2417192079@qq.com>
2017-09-28 14:41:31 +0800

    20170928---QMessageBox使用 question，information框

--- qthelloworld/qthelloworld.cpp
+++ qthelloworld/qthelloworld.cpp
@@ -34,6 +34,22 @@ qthelloworld::~qthelloworld()
 void qthelloworld::open()
 {
 	//QMessageBox::information(this, tr("Information"), tr("Open"));
+#if 1
+	//exp:对话框
+	if (QMessageBox::Yes == QMessageBox::question(this,
+		tr("Question"),
+		tr("Are you OK?"),
+		QMessageBox::Yes | QMessageBox::No,
+		QMessageBox::Yes)) // this，即 qthelloworld（或者其他 QWidget 指针）
+	{
+		QMessageBox::information(this, tr("Hmmm..."), tr("I'm glad to hear that!"));
+	}
+	else 
+	{
+		QMessageBox::information(this, tr("Hmmm..."), tr("I'm sorry!"));
+	}
+#endif
+
 #if 0
 	//QDialog dialog;
 	QDialog dialog(this);
@@ -41,7 +57,7 @@ void qthelloworld::open()
 	dialog.exec();//模态对话框，会阻塞同一应用程序中其它窗口的输入
 	//dialog.show();//在栈上建立，show()函数返回，MainWindow::open()函数结束，dialog 超出作用域被析构，因此对话框消失
 #endif
-#if 1
+#if 0
 	QDialog *dialog = new QDialog;
 	dialog->setWindowTitle(tr("Hello, dialog!"));
 	dialog->show();//改为在堆上建立  非模态对话框   没有delete，存在内存泄漏
