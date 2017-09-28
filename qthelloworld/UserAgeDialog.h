36f5c7779bd99d3dee3c317df48ec502a1a1bb2a
yexiaoqi <2417192079@qq.com>
2017-09-28 15:52:00 +0800

    20170928---可以运行，但还未用QLineEdit实现输入年龄功能

--- qthelloworld/UserAgeDialog.h
+++ qthelloworld/UserAgeDialog.h
@@ -8,7 +8,8 @@ class UserAgeDialog:public QDialog
 {
 	Q_OBJECT
 public:
-	UserAgeDialog(QWidget *parent = 0);
+	UserAgeDialog(QWidget *parent) {}
+	//UserAgeDialog(int newage) :newAge(newage) {}
 	~UserAgeDialog();
 	signals:
 		void userAgeChanged(int newage);
